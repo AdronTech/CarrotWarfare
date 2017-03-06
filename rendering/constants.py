e14842537269dcec4146e964c966dd7733000b96
TheMejmun <saman14m@gmail.com>
2017-03-06 16:57:45 +0100

    Updated Colors

--- rendering/constants.py
+++ rendering/constants.py
@@ -1,2 +1,3 @@
-# Light Blue, Orange, Teal, Pink @ Material Palette
-COLOR_PLAYERS = [(3, 169, 244), (255, 152, 0), (0, 150, 136), (233, 30, 99)]
+# Blue, Red, Green, Amber @ Material Palette
+COLOR_PLAYERS = [(33, 150, 243), (244, 67, 54), (76, 175, 80), (255, 193, 7)]
+COLOR_BACKGROUND = (250, 250, 250)
