9bb11e5eba03c39f3bfc3644a8f3454a0d203cdd
LEM-II\Ilja <>
2017-03-06 17:17:47 +0100

    world fix

--- game/world.py
+++ game/world.py
@@ -7,19 +7,10 @@ WORLD_DIMENSION = (20, 20)
 class World:
 
     def __init__(self):
-<<<<<<< HEAD
-        self.grid = [[Tile for i in range(WORLD_DIMENSION[1])] for j in range(WORLD_DIMENSION[0])]
         self.player_count = 0
-=======
-        self.grid = [[Tile() for i in range(WORLD_DIMENSION[1])] for j in range(WORLD_DIMENSION[0])]  # type: Tile[][]
->>>>>>> b53f980a90252725c2c614113f1f0ab38e102788
+        self.grid = [[Tile() for i in range(WORLD_DIMENSION[1])] for j in range(WORLD_DIMENSION[0])]  # type: [[Tile]]
         self.entities = []
         self.events = []
-
-        ent = Entity()
-        self.entities.append(ent)
-
-        self.grid[3][5].register(ent)
 
     def update(self):
 
