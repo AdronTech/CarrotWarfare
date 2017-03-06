52e51fb846816a91e347ca97c05fa2762c3a7f53
AdronTech <markus.goegele@hotmail.com>
2017-03-06 16:06:22 +0100

    Registered Entity

--- game/world.py
+++ game/world.py
@@ -1,4 +1,5 @@
 from game.tile import Tile
+from game.entity import Entity
 
 WORLD_DIMENSION = (20, 20)
 
@@ -6,9 +7,14 @@ WORLD_DIMENSION = (20, 20)
 class World:
 
     def __init__(self):
-        self.grid = [[Tile for i in range(WORLD_DIMENSION[1])] for j in range(WORLD_DIMENSION[0])]
+        self.grid = [[Tile() for i in range(WORLD_DIMENSION[1])] for j in range(WORLD_DIMENSION[0])]  # type: Tile[][]
         self.entities = []
         self.events = []
+
+        ent = Entity()
+        self.entities.append(ent)
+
+        self.grid[3][5].register(ent)
 
     def update(self):
 
--- rendering/renderer.py
+++ rendering/renderer.py
@@ -1,17 +1,19 @@
 from pygame import Surface, Rect, draw
 from game.world import WORLD_DIMENSION
 from display import RESOLUTION
+from game.tile import Tile
 
 TILESIZE = (RESOLUTION[0] / WORLD_DIMENSION[0],
             RESOLUTION[1] / WORLD_DIMENSION[1])
 
 CLR = {
     "black": (0, 0, 0),
-    "white": (255, 255, 255)
+    "white": (255, 255, 255),
+    "red": (255, 0, 0)
 }
 
 
-class AbstractRenderer:
+class AbstractRenderer :
     def render(self, target, world):
         pass
 
@@ -23,7 +25,18 @@ class TestRenderer (AbstractRenderer):
         target.fill(CLR["black"])
 
         for x in range(WORLD_DIMENSION[0]):
-            draw.line(target, CLR["white"], (x * TILESIZE[0], 0), (x * TILESIZE[0], RESOLUTION[1]))
+            for y in range(WORLD_DIMENSION[1]):
 
-        for y in range(WORLD_DIMENSION[0]):
-            draw.line(target, CLR["white"], (0, y * TILESIZE[1]), (RESOLUTION[0], y * TILESIZE[1]))
+                col = CLR["white"]
+
+                t = world.grid[x][y]  # type: Tile
+                if t.entities:
+                    col = CLR["red"]
+
+                draw.rect(target, col, Rect((x*TILESIZE[0], y*TILESIZE[1]), TILESIZE))
+
+        # for x in range(WORLD_DIMENSION[0]):
+        #     draw.line(target, CLR["white"], (x * TILESIZE[0], 0), (x * TILESIZE[0], RESOLUTION[1]))
+        #
+        # for y in range(WORLD_DIMENSION[1]):
+        #     draw.line(target, CLR["white"], (0, y * TILESIZE[1]), (RESOLUTION[0], y * TILESIZE[1]))
