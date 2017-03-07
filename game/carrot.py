60de992067a879c617803ed5342e2483e9c2aaa2
AdronTech <markus.goegele@hotmail.com>
2017-03-07 14:27:52 +0100

    Carrot advanced

--- game/carrot.py
+++ game/carrot.py
@@ -3,10 +3,11 @@ from enum import Enum
 from game.world import World
 from game.player import Player
 
+
 class Carrot(Entity):
 
-    def __init__(self, world: World, player: Player):
-        super().__init__(world)
+    def __init__(self, world: World, player: Player, pos):
+        super().__init__(world, pos)
         self.state = CarrotState.idle
         self.player = player
 
@@ -14,29 +15,36 @@ class Carrot(Entity):
         self.player_range = 1
         self.enemy_range = 1
 
-        self.entities_in_sight = []
+        self.entities_in_sight = []  # type: list[Entity]
+        self.target = self  # type: Entity
 
-    def update(self):
+    def update(self, events=None, input=None):
         if self.state is CarrotState.idle:
 
-            self.look_arround()
+            self.look_around()
 
             if self.enemy_nearby():
                 self.target = self.get_nearest_enemy()
-                self.state = CarrotState.seek_enemy
+                if self.target:
+                    self.state = CarrotState.seek_enemy
 
-    def look_arround(self):
+        elif self.state is CarrotState.seek_enemy:
+            if self.target:
+                self.target.
+
+
+    def look_around(self):
         self.entities_in_sight = []
 
-        myX = int(self.pos.x)
-        myY = int(self.pos.y)
+        my_x = int(self.pos.x)
+        my_y = int(self.pos.y)
 
-        for x in range(myX - self.sight_range, myX + self.sight_range + 1):
-            for y in range(myY - self.sight_range, myY + self.sight_range + 1):
-                if x - myX + y - myY > self.sight_range:
+        for x in range(my_x - self.sight_range, my_x + self.sight_range + 1):
+            for y in range(my_y - self.sight_range, my_y + self.sight_range + 1):
+                if x - my_x + y - my_y > self.sight_range:
                     continue
 
-                t = self.world.grid[myX][myY]
+                t = self.world.grid[my_x][my_y]
                 self.entities_in_sight.extend(t.entities)
 
     def enemy_nearby(self):
@@ -45,9 +53,19 @@ class Carrot(Entity):
                 return True
 
     def get_nearest_enemy(self):
+        if not self.entities_in_sight:
+            return None
 
-        # TODO: find nearest
-        pass
+        index = 0
+        for i in range(1, len(self.entities_in_sight)):
+            if self.entities_in_sight[i].pos.distance_squared_to(self.pos) < self.entities_in_sight[index].pos.distance_squared_to(self.pos):
+                index = i
+
+        return index
+
+    def call(self):
+        self.target = self.player
+        self.state = CarrotState.seek_player
 
 
 class CarrotState(Enum):
