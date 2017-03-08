328b3a23a36768bf2986b3f7a5673dcc60c9630e
TheMejmun <saman14m@gmail.com>
2017-03-08 19:20:11 +0100

    ui fix

--- rendering/ultimate_version/ui_layer.py
+++ rendering/ultimate_version/ui_layer.py
@@ -6,7 +6,7 @@ class UILayer:
         self.parent_renderer = renderer
         self.player_ui_surfaces = player_ui_surfaces
 
-        self.max_health = 40.0
+        self.max_health = 100.0
         self.max_seeds = 200.0
 
         self.current_health = 1.0
@@ -23,52 +23,27 @@ class UILayer:
         self.current_seed_count_melee = player.get_seeds(SeedType.melee) / self.max_seeds
         self.current_seed_count_ranged = player.get_seeds(SeedType.ranged) / self.max_seeds
 
-        # ask Saman for advice
-        # minimize
-        def draw(bottom: bool):
-            if not bottom:
-                target.fill(COLOR_PLAYERS[i], ((0, 0), (target.get_width(), target.get_height() * self.current_health)))
-                if player.seed_mode == SeedType.melee:
-                    target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[0]),
-                                                   (UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
-                    target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0] * 2,
-                                                              UI_SEED_SURFACE_SIZE[0]),
-                                                             (UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[
-                                                                  1] * self.current_seed_count_ranged)))
-                elif player.seed_mode == SeedType.ranged:
-                    target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[0]),
-                                                             (UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
-                    target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0] * 2,
-                                                    UI_SEED_SURFACE_SIZE[0]),
-                                                   (UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_ranged)))
-            else:
-                if player.seed_mode == SeedType.melee:
-                    target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[0]),
-                                                   (UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
-                    target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0] * 2,
-                                                              UI_SEED_SURFACE_SIZE[0]),
-                                                             (UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[
-                                                                  1] * self.current_seed_count_ranged)))
-                elif player.seed_mode == SeedType.ranged:
-                    target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[0]),
-                                                             (UI_SEED_SURFACE_SIZE[0],
-                                                              UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
-                    target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0] * 2,
-                                                    UI_SEED_SURFACE_SIZE[0]),
-                                                   (UI_SEED_SURFACE_SIZE[0],
-                                                    UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_ranged)))
+        target.fill(COLOR_PLAYERS[i], ((0, 0), (target.get_width(), target.get_height() * self.current_health)))
 
-        draw(False)  # i == 0 or i == 1
+        if player.seed_mode == SeedType.melee:
+            target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0],
+                                            UI_SEED_SURFACE_SIZE[0]),
+                                           (UI_SEED_SURFACE_SIZE[0],
+                                            UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
+            target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0] * 2,
+                                                      UI_SEED_SURFACE_SIZE[0]),
+                                                     (UI_SEED_SURFACE_SIZE[0],
+                                                      UI_SEED_SURFACE_SIZE[
+                                                          1] * self.current_seed_count_ranged)))
+        elif player.seed_mode == SeedType.ranged:
+            target.fill(COLOR_PLAYERS_SECONDARY[i], ((UI_SEED_SURFACE_SIZE[0],
+                                                      UI_SEED_SURFACE_SIZE[0]),
+                                                     (UI_SEED_SURFACE_SIZE[0],
+                                                      UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_melee)))
+            target.fill(COLOR_BACKGROUND, ((UI_SEED_SURFACE_SIZE[0] * 2,
+                                            UI_SEED_SURFACE_SIZE[0]),
+                                           (UI_SEED_SURFACE_SIZE[0],
+                                            UI_SEED_SURFACE_SIZE[1] * self.current_seed_count_ranged)))
 
     def render(self, world: World):
         for i in range(world.player_count):
